2d86c4ce79a9e02d0b2d91ab370ad57947316027
Ebit Gunawan <ebitgunawan2@gmail.com>
2024-12-10 13:43:24 +0700

    update3

--- coba-upload/api/delete.js
+++ coba-upload/api/delete.js
@@ -1,35 +1,38 @@
 export default async function handler(req, res) {
-	if (req.method !== 'POST') {
-		return res.status(405).json({ error: 'Method not allowed' });
-	}
-
-	try {
+	if (req.method === 'POST') {
 		const { publicId } = req.body;
 
 		if (!publicId) {
 			return res.status(400).json({
-				error: 'Public ID is required',
+				error: 'publicId is required',
 			});
 		}
 
-		// Konfigurasi Cloudinary
-		const cloudinary = require('cloudinary').v2;
-		cloudinary.config({
-			cloud_name: 'dhei3po2m',
-			api_key: '684285221384396',
-			api_secret: 'sfr0Xa79bgV4NQKq1xJ0tYOgnYA',
-		});
+		try {
+			// Logika untuk menghapus gambar dari Cloudinary
+			const cloudinary = require('cloudinary').v2;
+			cloudinary.config({
+				cloud_name: 'dhei3po2m',
+				api_key: '684285221384396',
+				api_secret: 'sfr0Xa79bgV4NQKq1xJ0tYOgnYA',
+			});
 
-		// Hapus file dari Cloudinary
-		const result = await cloudinary.uploader.destroy(publicId);
+			const response = await cloudinary.uploader.destroy(
+				publicId
+			);
 
-		if (result.result !== 'ok') {
-			throw new Error('Failed to delete the image');
+			if (response.result === 'ok') {
+				res.status(200).json({ success: true });
+			} else {
+				throw new Error(
+					'Gagal menghapus gambar.'
+				);
+			}
+		} catch (error) {
+			res.status(500).json({ error: error.message });
 		}
-
-		res.status(200).json({ message: 'Image deleted successfully' });
-	} catch (error) {
-		console.error('Error deleting image:', error);
-		res.status(500).json({ error: 'Internal server error' });
+	} else {
+		res.setHeader('Allow', ['POST']);
+		res.status(405).end(`Method ${req.method} Not Allowed`);
 	}
 }
